7eca7f80e88f4fc89c67d55d28a3cc4ad755a7dc
Pradeep <pradeep.vrs@gmail.com>
2023-03-19 16:03:40 +0530

    Xcode setup

---
+++ NASA Daily Pics/NASA_Daily_PicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASA_Daily_PicsApp.swift
+//  NASA Daily Pics
+//
+//  Created by APPLE on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct NASA_Daily_PicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NASA Daily PicsUITests/NASA_Daily_PicsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NASA_Daily_PicsUITestsLaunchTests.swift
+//  NASA Daily PicsUITests
+//
+//  Created by APPLE on 19/03/23.
+//
+
+import XCTest
+
+final class NASA_Daily_PicsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
